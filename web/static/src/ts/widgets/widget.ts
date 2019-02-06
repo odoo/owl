49ecc05297cd29a5eaf8b5424c39e869151171d1
Géry Debongnie <ged@odoo.com>
2019-02-06 10:15:07 +0100

    make purewidget reactive on isMobile key change

--- web/static/src/ts/widgets/widget.ts
+++ web/static/src/ts/widgets/widget.ts
@@ -33,8 +33,16 @@ export interface Env extends WEnv {
 
 export class Widget<Props, State> extends Component<Env, Props, State> {}
 
-export class PureWidget<Props, State> extends PureComponent<
-  Env,
-  Props,
-  State
-> {}
+export class PureWidget<P, S> extends PureComponent<Env, P, S> {
+  constructor(parent, props) {
+    super(parent, props);
+    (<any>this.__widget__).isMobile = this.env.isMobile;
+  }
+  shouldUpdate(nextProps: P): boolean {
+    if ((<any>this.__widget__).isMobile !== this.env.isMobile) {
+      (<any>this.__widget__).isMobile = this.env.isMobile;
+      return true;
+    }
+    return super.shouldUpdate(nextProps);
+  }
+}

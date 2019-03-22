a444e3dcaca87445a057bf7be1c2adfa628be986
Géry Debongnie <ged@odoo.com>
2019-03-22 12:08:55 +0100

    imp: export utils in main bundle

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { QWeb } from "./qweb";
 import { EventBus } from "./event_bus";
 import { Component, PureComponent } from "./component";
 import { Store, connect } from "./store";
-
+import * as utils from "./utils";
 export const core = {
   QWeb,
   EventBus,
   Component,
   PureComponent,
   Store,
-  connect
+  connect,
+  utils
 };

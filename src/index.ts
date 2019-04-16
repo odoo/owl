eac49562fa210379968c89c27c637da714b023d3
Géry Debongnie <ged@odoo.com>
2019-04-16 16:26:06 +0200

    [REF] owl: export owl elements in one object

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
-import { Component, PureComponent } from "./component";
-import { EventBus } from "./event_bus";
-import { QWeb } from "./qweb";
-import { Registry } from "./registry";
-import { connect, Store } from "./store";
-import * as utils from "./utils";
+export { Component, PureComponent } from "./component";
+export { EventBus } from "./event_bus";
+export { QWeb } from "./qweb";
+export { Registry } from "./registry";
+export { connect, Store } from "./store";
+import * as _utils from "./utils";
 
-export const core = {
-  QWeb,
-  EventBus,
-  Component,
-  PureComponent,
-  utils
-};
-
-export const extras = {
-  Store,
-  connect,
-  Registry
-};
+export const utils = _utils;

d07f3965787f94b0297568ad754233da8d80860b
Géry Debongnie <ged@odoo.com>
2019-04-18 07:40:27 +0200

    [REF] move jest.config.js into package.json

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
-  transform: {
-    "^.+\\.ts?$": "ts-jest"
-  },
-  verbose: false,
-  testRegex: "(/tests/.*(test|spec))\\.ts?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-};

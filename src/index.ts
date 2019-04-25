40613e0a76d8a5813845273eb20b4d337b0eb654
Géry Debongnie <ged@odoo.com>
2019-04-25 15:32:56 +0200

    [IMP] observer: add observer to exported objects

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Component } from "./component";
 export { EventBus } from "./event_bus";
+export { Observer } from "./observer";
 export { QWeb } from "./qweb";
 export { connect, Store } from "./store";
 import * as _utils from "./utils";

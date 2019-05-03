10ab97e3c6fffff3c100e53ac6bdc6c77f7c8d13
Géry Debongnie <ged@odoo.com>
2019-05-03 11:14:34 +0200

    [REF] extras: improve code

--- extras/server.py
+++ extras/server.py
@@ -3,18 +3,32 @@ import thread
 import webbrowser
 import time
 
-import BaseHTTPServer, SimpleHTTPServer
+import BaseHTTPServer
+import SimpleHTTPServer
+
+HOST = '127.0.0.1'
+PORT = 8000
+
+
+class OWLHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/extras/owl.js':
+            self.path = '/dist/owl.js'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
 
 def start_server():
-    httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', 3600), SimpleHTTPServer.SimpleHTTPRequestHandler)
+    httpd = BaseHTTPServer.HTTPServer((HOST, PORT), OWLHandler)
     httpd.serve_forever()
 
-thread.start_new_thread(start_server,())
-url = 'http://127.0.0.1:3600/extras'
-webbrowser.open_new(url)
 
-while True:
-    try:
-        time.sleep(1)
-    except KeyboardInterrupt:
-        sys.exit(0)
+if __name__ == "__main__":
+    thread.start_new_thread(start_server, ())
+    url = 'http://{0}:{1}/extras'.format(HOST, PORT)
+    webbrowser.open_new(url)
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            sys.exit(0)

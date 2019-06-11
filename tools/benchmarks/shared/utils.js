6659375e23bc8c46c005e4435c3548bb919712ad
Géry Debongnie <ged@odoo.com>
2019-06-11 14:44:30 +0200

    [IMP] tools: rework benchmarks application
    closes #130

--- tools/benchmarks/shared/utils.js
+++ tools/benchmarks/shared/utils.js
@@ -31,19 +31,28 @@ export function buildData(n = 1000) {
 //------------------------------------------------------------------------------
 // Measuring helpers
 //------------------------------------------------------------------------------
+export function formatNumber(n) {
+    return Number(n).toFixed();
+}
+
 let startTime;
 let lastMeasure;
 export function startMeasure(descr) {
   startTime = performance.now();
   lastMeasure = descr;
 }
-export function stopMeasure() {
+export function stopMeasure(cb) {
   let last = lastMeasure;
   if (lastMeasure) {
     window.setTimeout(function() {
       lastMeasure = null;
       const stop = performance.now();
-      console.log(`[${last}] took  ${Number(stop - startTime).toFixed()}ms`);
+      const delta = stop - startTime;
+      const msg = `[${last}] took  ${formatNumber(delta)}ms`;
+      console.log(msg);
+      if (cb) {
+          cb({msg, delta});
+      }
     }, 0);
   }
 }

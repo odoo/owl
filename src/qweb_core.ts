d96b7960a74ffcad2d0f6e0ce8326c1d2f5fbd14
Mathieu Duckerts-Antoine <dam@odoo.com>
2019-06-28 13:02:52 +0200

    [IMP] qweb,core: better msg error for bad xml templates

--- src/qweb_core.ts
+++ src/qweb_core.ts
@@ -117,7 +117,27 @@ function parseXML(xml: string): Document {
   const parser = new DOMParser();
   const doc = parser.parseFromString(xml, "text/xml");
   if (doc.getElementsByTagName("parsererror").length) {
-    throw new Error("Invalid XML in template");
+    let msg = "Invalid XML in template."
+    const parsererrorText = doc.getElementsByTagName("parsererror")[0].textContent;
+    if (parsererrorText) {
+      msg += "\n" + "The parser has produced the following error message:\n" + parsererrorText;
+      const re = /\d+/g;
+      const firstMatch = re.exec(parsererrorText);
+      if (firstMatch) {
+        const lineNumber = Number(firstMatch[0]);
+        const line = xml.split("\n")[lineNumber - 1];
+        const secondMatch = re.exec(parsererrorText);
+        if (line && secondMatch) {
+            const columnIndex = Number(secondMatch[0]) - 1;
+            if (line[columnIndex]) {
+              msg += "\n" + `The error might be located at xml line ${lineNumber} column ${columnIndex}\n` +
+                      line + "\n" +
+                      "-".repeat(columnIndex - 1) + "^";
+            }
+        }
+      }
+    }
+    throw new Error(msg);
   }
   return doc;
 }

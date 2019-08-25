bb4c2506c08594e2160bf4a1b576693039684a46
Géry Debongnie <ged@odoo.com>
2019-08-25 21:38:16 +0200

    [REF] store: merge actions and mutations
    closes #256

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,7 +11,7 @@ import { Observer } from "../core/observer";
  *
  * The Owl store is our answer to the problem of managing complex state across
  * components. The main idea is that the store owns some state, allow external
- * code to modify it through actions/mutations, and for each state changes,
+ * code to modify it through actions, and for each state changes,
  * connected component will be notified, and updated if necessary.
  *
  * Note that this code is partly inspired by VueX and React/Redux
@@ -21,16 +21,14 @@ import { Observer } from "../core/observer";
 // Store Definition
 //------------------------------------------------------------------------------
 
-type Mutation = ({ state, commit, getters }, ...payload: any) => void;
-type Action = ({ commit, state, dispatch, env, getters }, ...payload: any) => void;
-type Getter = ({ state, getters }, payload) => any;
+export type Action = ({ state, dispatch, env, getters }, ...payload: any) => any;
+export type Getter = ({ state: any, getters }, payload?) => any;
 
 interface StoreConfig {
   env?: Env;
   state?: any;
   actions?: { [name: string]: Action };
   getters?: { [name: string]: Getter };
-  mutations?: { [name: string]: Mutation };
 }
 
 interface StoreOption {
@@ -41,46 +39,28 @@ export class Store extends EventBus {
   state: any;
   actions: any;
   mutations: any;
-  _commitLevel: number = 0;
-  history: any[] = [];
   debug: boolean;
   env: any;
   observer: Observer;
-  getters: { [name: string]: (payload?) => any };
-  _gettersCache: { [name: string]: {} };
-  _updateId: number = 1;
+    getters: { [name: string]: (payload?) => any };
 
   constructor(config: StoreConfig, options: StoreOption = {}) {
     super();
     this.debug = options.debug || false;
     this.actions = config.actions;
-    this.mutations = config.mutations;
     this.env = config.env;
     this.observer = new Observer();
     this.observer.notifyCB = this.__notifyComponents.bind(this);
-    this.observer.allowMutations = false;
     this.state = this.observer.observe(config.state || {});
     this.getters = {};
-    this._gettersCache = {};
-
-    if (this.debug) {
-      this.history.push({ state: this.state });
-    }
-
-    const cTypes = ["undefined", "number", "string"];
-    for (let entry of Object.entries(config.getters || {})) {
-      const name: string = entry[0];
-      const func: (...any) => any = entry[1];
-      this.getters[name] = payload => {
-        if (this._commitLevel === 0 && cTypes.indexOf(typeof payload) >= 0) {
-          this._gettersCache[name] = this._gettersCache[name] || {};
-          this._gettersCache[name][payload] =
-            this._gettersCache[name][payload] ||
-            func({ state: this.state, getters: this.getters }, payload);
-          return this._gettersCache[name][payload];
+    if (config.getters) {
+        const firstArg = {
+            state: this.state,
+            getters: this.getters,
+        };
+        for (let g in config.getters) {
+            this.getters[g] = config.getters[g].bind(this, firstArg);
         }
-        return func({ state: this.state, getters: this.getters }, payload);
-      };
     }
   }
 
@@ -90,7 +70,6 @@ export class Store extends EventBus {
     }
     const result = this.actions[action](
       {
-        commit: this.commit.bind(this),
         dispatch: this.dispatch.bind(this),
         env: this.env,
         state: this.state,
@@ -99,37 +78,6 @@ export class Store extends EventBus {
       ...payload
     );
     return result;
-  }
-
-  commit(type: string, ...payload: any): any {
-    if (!this.mutations[type]) {
-      throw new Error(`[Error] mutation ${type} is undefined`);
-    }
-    this._commitLevel++;
-    this.observer.allowMutations = true;
-
-    const res = this.mutations[type].call(
-      null,
-      {
-        commit: this.commit.bind(this),
-        state: this.state,
-        getters: this.getters
-      },
-      ...payload
-    );
-
-    if (this._commitLevel === 1) {
-      this.observer.allowMutations = false;
-      if (this.debug) {
-        this.history.push({
-          state: this.state,
-          mutation: type,
-          payload: [...payload]
-        });
-      }
-    }
-    this._commitLevel--;
-    return res;
   }
 
   /**
@@ -147,15 +95,12 @@ export class Store extends EventBus {
    * updating all widgets concurrently, except for parents/children.
    */
   async __notifyComponents() {
-    this._updateId++;
-    const current = this._updateId;
-    this._gettersCache = {};
     const subs = this.subscriptions.update || [];
     for (let i = 0, iLen = subs.length; i < iLen; i++) {
       const sub = subs[i];
       const shouldCallback = sub.owner ? sub.owner.__owl__.isMounted : true;
       if (shouldCallback) {
-        await sub.callback.call(sub.owner, current);
+        await sub.callback.call(sub.owner);
       }
     }
   }
--- tests/store/store.test.ts
+++ tests/store/store.test.ts
@@ -1,57 +1,33 @@
 import { Env } from "../../src/component/component";
-import { Store } from "../../src/store/store";
-import { nextMicroTick, nextTick } from "../helpers";
-
+import { Store, Getter } from "../../src/store/store";
+import { nextTick, nextMicroTick } from "../helpers";
 
 describe("basic use", () => {
-  test("commit a mutation", () => {
-    const state = { n: 1 };
-    const mutations = {
-      inc({ state }, delta) {
-        state.n += delta;
-      }
-    };
-    const store = new Store({ state, mutations });
-
-    expect(store.state.n).toBe(1);
-    store.commit("inc", 14);
-    expect(store.state.n).toBe(15);
-  });
-
   test("dispatch an action", () => {
     const state = { n: 1 };
-    const mutations = {
+    const actions = {
       inc({ state }, delta) {
         state.n += delta;
       }
     };
-    const actions = {
-      inc({ commit }, delta) {
-        commit("inc", delta);
-      }
-    };
-    const store = new Store({ state, mutations, actions });
+    const store = new Store({ state, actions });
 
     expect(store.state.n).toBe(1);
     store.dispatch("inc", 14);
     expect(store.state.n).toBe(15);
   });
 
-  test("dispatch an action + commit a mutation with positional arguments", () => {
+  test("dispatch an action with positional arguments", () => {
     const state = { n1: 1, n2: 1, n3: 1 };
-    const mutations = {
+
+    const actions = {
       batchInc({ state }, delta1, delta2, delta3) {
         state.n1 += delta1;
         state.n2 += delta2;
         state.n3 += delta3;
       }
     };
-    const actions = {
-      batchInc({ commit }, delta1, delta2, delta3) {
-        commit("batchInc", delta1, delta2, delta3);
-      }
-    };
-    const store = new Store({ state, mutations, actions });
+    const store = new Store({ state, actions });
 
     expect(store.state.n1).toBe(1);
     expect(store.state.n2).toBe(1);
@@ -62,96 +38,58 @@ describe("basic use", () => {
     expect(store.state.n3).toBe(89);
   });
 
-  test("modifying state outside of mutations trigger error", () => {
-    const state = { n: 1 };
-    const actions = {
-      inc({ state }) {
-        state.n++;
-      }
-    };
-    const store = new Store({ state, mutations: {}, actions });
-
-    expect(() => store.dispatch("inc")).toThrow();
-    expect(() => (store.state.n = 15)).toThrow();
-  });
-
   test("can dispatch an action in an action", () => {
     const state = { n: 1 };
-    const mutations = {
+
+    const actions = {
       inc({ state }, delta) {
         state.n += delta;
-      }
-    };
-    const actions = {
-      inc({ commit }, delta) {
-        commit("inc", delta);
       },
       inc100({ dispatch }) {
         dispatch("inc", 100);
       }
     };
-    const store = new Store({ state, mutations, actions });
+    const store = new Store({ state, actions });
 
     expect(store.state.n).toBe(1);
     store.dispatch("inc100");
     expect(store.state.n).toBe(101);
   });
 
-  test("can commit a mutation in a mutation", () => {
+  test("return data from dispatching an action", () => {
     const state = { n: 1 };
-    const mutations = {
-      inc({ state }) {
-        state.n++;
-      },
-      inc10({ commit }) {
-        for (let i = 0; i < 10; i++) {
-          commit("inc");
-        }
-      }
-    };
-    const store = new Store({ state, mutations });
-
-    expect(store.state.n).toBe(1);
-    store.commit("inc10");
-    expect(store.state.n).toBe(11);
-  });
-
-  test("return data from committing a mutation", () => {
-    const state = { n: 1 };
-    const mutations = {
+    const actions = {
       inc({ state }) {
         return ++state.n;
       }
     };
-    const store = new Store({ state, mutations });
+    const store = new Store({ state, actions });
 
     expect(store.state.n).toBe(1);
-    const res = store.commit("inc");
+    const res = store.dispatch("inc");
     expect(store.state.n).toBe(2);
     expect(res).toBe(2);
   });
 
   test("dispatch allow synchronizing between actions", async () => {
     const state = { n: 1 };
-    const mutations = {
+    const actions = {
       inc({ state }, delta) {
         state.n += delta;
       },
       setN({ state }, n) {
         state.n = n;
-      }
-    };
-    const actions = {
-      async dosomething({ commit, dispatch }) {
-        await dispatch("setTo10");
-        commit("inc", 3);
       },
-      async setTo10({ commit }) {
+      async dosomething({ dispatch }) {
+        await dispatch("setTo10");
+        dispatch("inc", 3);
+      },
+      async setTo10({ dispatch }) {
         await Promise.resolve();
-        commit("setN", 10);
+        dispatch("setN", 10);
       }
     };
-    const store = new Store({ state, mutations, actions });
+    const store = new Store({ state, actions });
 
     expect(store.state.n).toBe(1);
     store.dispatch("dosomething");
@@ -162,26 +100,24 @@ describe("basic use", () => {
 
   test("return data from dispatching an action", async () => {
     const state = { n: 1 };
-    const mutations = {
+    const actions = {
       inc({ state }, delta) {
         state.n += delta;
       },
       setN({ state }, n) {
         state.n = n;
-      }
-    };
-    const actions = {
-      async dosomething({ commit, dispatch }) {
-        const val = await dispatch("setTo10");
-        commit("inc", val);
       },
-      async setTo10({ commit }) {
+      async dosomething({ dispatch }) {
+        const val = await dispatch("setTo10");
+        dispatch("inc", val);
+      },
+      async setTo10({ dispatch }) {
         await Promise.resolve();
-        commit("setN", 10);
+        dispatch("setN", 10);
         return 5;
       }
     };
-    const store = new Store({ state, mutations, actions });
+    const store = new Store({ state, actions });
 
     expect(store.state.n).toBe(1);
     store.dispatch("dosomething");
@@ -219,7 +155,7 @@ describe("basic use", () => {
         }
       }
     };
-    const getters = {
+    const getters: { [key: string]: Getter } = {
       beerTasterName({ state }, beerID) {
         return state.tasters[state.beers[beerID].tasterID].name;
       },
@@ -227,159 +163,10 @@ describe("basic use", () => {
         return state.beers[1].name;
       }
     };
-    const store = new Store({ state, mutations: {}, actions: {}, getters });
+    const store = new Store({ state, actions: {}, getters });
     expect(store.getters).toBeDefined();
     expect((<any>store.getters).bestBeerName()).toBe("bertinchamps");
     expect((<any>store.getters).beerTasterName(1)).toBe("aaron");
-  });
-
-  test("getters are memoized", async () => {
-    const state = {
-      beers: {
-        1: {
-          id: 1,
-          name: "bertinchamps",
-          tasterID: 1
-        }
-      },
-      tasters: {
-        1: {
-          id: 1,
-          name: "aaron"
-        }
-      }
-    };
-    let n = 0;
-    const getters = {
-      beerTasterName({ state }, beerID) {
-        n++;
-        return state.tasters[state.beers[beerID].tasterID].name;
-      },
-      bestBeerName({ state }) {
-        n++;
-        return state.beers[1].name;
-      }
-    };
-    const store = new Store({ state, mutations: {}, actions: {}, getters });
-    expect((<any>store.getters).bestBeerName()).toBe("bertinchamps");
-    expect((<any>store.getters).beerTasterName(1)).toBe("aaron");
-    expect(n).toBe(2);
-    expect((<any>store.getters).bestBeerName()).toBe("bertinchamps");
-    expect((<any>store.getters).beerTasterName(1)).toBe("aaron");
-    expect(n).toBe(2);
-  });
-
-  test("getters taking Array as argument aren't memoized", async () => {
-    const state = {
-      beers: {
-        1: {
-          id: 1,
-          name: "bertinchamps",
-          tasterID: 1
-        }
-      }
-    };
-    let n = 0;
-    const getters = {
-      getBeerNames({ state }, beerIDs) {
-        n++;
-        return beerIDs.map(beerID => {
-          return state.beers[beerID].name;
-        });
-      }
-    };
-    const store = new Store({ state, mutations: {}, actions: {}, getters });
-    expect((<any>store.getters).getBeerNames([1])).toEqual(["bertinchamps"]);
-    expect(n).toBe(1);
-    expect((<any>store.getters).getBeerNames([1])).toEqual(["bertinchamps"]);
-    expect(n).toBe(2);
-  });
-
-  test("getters cache is nuked on store changes", async () => {
-    const state = {
-      beers: {
-        1: {
-          id: 1,
-          name: "bertinchamps",
-          tasterID: 1
-        }
-      },
-      tasters: {
-        1: {
-          id: 1,
-          name: "aaron"
-        },
-        2: {
-          id: 2,
-          name: "gery"
-        }
-      }
-    };
-    const mutations = {
-      changeTaster({ state }, { beerID, tasterID }) {
-        state.beers[beerID].tasterID = tasterID;
-      }
-    };
-    let n = 0;
-    const getters = {
-      beerTasterName({ state }, beerID) {
-        n++;
-        return state.tasters[state.beers[beerID].tasterID].name;
-      }
-    };
-    const store = new Store({
-      state,
-      mutations: mutations,
-      actions: {},
-      getters
-    });
-    expect((<any>store.getters).beerTasterName(1)).toBe("aaron");
-    expect(n).toBe(1);
-    expect((<any>store.getters).beerTasterName(1)).toBe("aaron");
-    expect(n).toBe(1);
-
-    store.commit("changeTaster", { beerID: 1, tasterID: 2 });
-    await nextTick();
-
-    expect((<any>store.getters).beerTasterName(1)).toBe("gery");
-    expect(n).toBe(2);
-  });
-
-  test("getters cache is disabled during a mutation", async () => {
-    const state = {
-      beers: {
-        1: {
-          id: 1,
-          name: "bertinchamps"
-        }
-      }
-    };
-    const mutations = {
-      renameBeer({ state, getters }, beerID) {
-        expect(getters.beerName(beerID)).toBe("bertinchamps");
-        state.beers[1].name = "chouffe";
-        expect(getters.beerName(beerID)).toBe("chouffe");
-      }
-    };
-    let n = 0;
-    const getters = {
-      beerName({ state }, beerID) {
-        n++;
-        return state.beers[beerID].name;
-      }
-    };
-    const store = new Store({
-      state,
-      mutations: mutations,
-      actions: {},
-      getters
-    });
-
-    store.commit("renameBeer", 1);
-    expect((<any>store.getters).beerName(1)).toBe("chouffe");
-    await nextTick();
-
-    expect(n).toBe(3);
   });
 
   test("getters given to actions", async () => {
@@ -414,44 +201,8 @@ describe("basic use", () => {
         expect(getters.beerTasterName(1)).toBe("aaron");
       }
     };
-    const store = new Store({ state, mutations: {}, actions, getters });
+    const store = new Store({ state, actions, getters });
     store.dispatch("action");
-  });
-
-  test("getters given to mutations", async () => {
-    expect.assertions(3);
-    const state = {
-      beers: {
-        1: {
-          id: 1,
-          name: "bertinchamps",
-          tasterID: 1
-        }
-      },
-      tasters: {
-        1: {
-          id: 1,
-          name: "aaron"
-        }
-      }
-    };
-    const getters = {
-      beerTasterName({ state }, beerID) {
-        return state.tasters[state.beers[beerID].tasterID].name;
-      },
-      bestBeerName({ state }) {
-        return state.beers[1].name;
-      }
-    };
-    const mutations = {
-      mutation({ getters }) {
-        expect(getters).toBeDefined();
-        expect(getters.bestBeerName()).toBe("bertinchamps");
-        expect(getters.beerTasterName(1)).toBe("aaron");
-      }
-    };
-    const store = new Store({ state, mutations, actions: {}, getters });
-    store.commit("mutation");
   });
 
   test("can use getters inside a getter", () => {
@@ -466,7 +217,7 @@ describe("basic use", () => {
         return `c${i}`;
       }
     };
-    const store = new Store({ getters });
+    const store = new Store({ getters, state: {}, actions: {} });
 
     expect(store.getters.a()).toBe("bc1");
   });
@@ -474,73 +225,73 @@ describe("basic use", () => {
 
 describe("advanced state properties", () => {
   test("state in the store is reference equal after mutation", async () => {
-    const mutations = {
+    const actions = {
       donothing() {}
     };
-    const store = new Store({ state:{}, mutations });
+    const store = new Store({ state: {}, actions });
     const state = store.state;
-    store.commit("donothing");
+    store.dispatch("donothing");
     expect(store.state).toBe(state);
   });
 
   test("can use array properties in mutations", () => {
     expect.assertions(3);
     const state = { a: [1, 2, 3] };
-    const mutations = {
+    const actions = {
       m({ state }) {
         expect(state.a.length).toBe(3);
         const l = state.a.push(53);
         expect(l).toBe(4);
       }
     };
-    const store = new Store({ state, mutations });
-    store.commit("m");
+    const store = new Store({ state, actions });
+    store.dispatch("m");
     expect(store.state.a).toEqual([1, 2, 3, 53]);
   });
 
-  test("can use object assign in store", async () => {
-    const mutations = {
-      dosomething({ state }) {
-        Object.assign(state.westmalle, { a: 3, b: 4 });
-      }
-    };
-    const store = new Store({
-      state: { westmalle: { a: 1, b: 2 } },
-      mutations
+    test("can use object assign in store", async () => {
+      const actions = {
+        dosomething({ state }) {
+          Object.assign(state.westmalle, { a: 3, b: 4 });
+        }
+      };
+      const store = new Store({
+        state: { westmalle: { a: 1, b: 2 } },
+        actions
+      });
+      store.dispatch("dosomething");
+      expect(store.state.westmalle).toEqual({ a: 3, b: 4 });
     });
-    store.commit("dosomething");
-    expect(store.state.westmalle).toEqual({ a: 3, b: 4 });
-  });
 
-  test("aku reactive store state 1", async () => {
-    const mutations = {
-      inc({ state }) {
-        state.counter++;
-      }
-    };
-    const state = { counter: 0 };
-    const store = new Store({ state, mutations });
-    expect(store.state.counter).toBe(0);
-    store.commit("inc", {});
-    expect(store.state.counter).toBe(1);
-  });
+    test("aku reactive store state 1", async () => {
+      const actions = {
+        inc({ state }) {
+          state.counter++;
+        }
+      };
+      const state = { counter: 0 };
+      const store = new Store({ state, actions });
+      expect(store.state.counter).toBe(0);
+      store.dispatch("inc", {});
+      expect(store.state.counter).toBe(1);
+    });
 });
 
 describe("updates triggered by the store", () => {
   test("multiple commits trigger one update", async () => {
     let updateCounter = 0;
     const state = { n: 1 };
-    const mutations = {
+    const actions = {
       inc({ state }, delta) {
         state.n += delta;
       }
     };
-    const store = new Store({ state, mutations });
+    const store = new Store({ state, actions });
     store.on("update", null, () => updateCounter++);
 
-    store.commit("inc", 14);
+    store.dispatch("inc", 14);
     expect(updateCounter).toBe(0);
-    store.commit("inc", 50);
+    store.dispatch("inc", 50);
     expect(updateCounter).toBe(0);
     await nextMicroTick();
     expect(updateCounter).toBe(1);
@@ -549,7 +300,7 @@ describe("updates triggered by the store", () => {
   test("empty commits do not trigger updates", async () => {
     let updateCounter = 0;
     const state = { n: 1 };
-    const mutations = {
+    const actions = {
       inc({ state }, delta) {
         state.n += delta;
       },
@@ -559,20 +310,19 @@ describe("updates triggered by the store", () => {
         state.n = val;
       }
     };
-    const store = new Store({ state, mutations });
+    const store = new Store({ state, actions });
     store.on("update", null, () => updateCounter++);
 
-    store.commit("noop");
+    store.dispatch("noop");
     await nextMicroTick();
     expect(updateCounter).toBe(0);
 
-    store.commit("inc", 50);
+    store.dispatch("inc", 50);
     await nextMicroTick();
     expect(updateCounter).toBe(1);
 
-    store.commit("noop2");
+    store.dispatch("noop2");
     await nextMicroTick();
     expect(updateCounter).toBe(1);
   });
 });
-

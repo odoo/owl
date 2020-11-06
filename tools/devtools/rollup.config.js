f8ba0e7c74bec2bc800e3fbcdd09b4fd41653d0e
Simon Genin (ges) <ges@odoo.com>
2020-11-06 12:08:27 +0100

    wip

--- tools/devtools/rollup.config.js
+++ tools/devtools/rollup.config.js
@@ -1,18 +1,18 @@
-import serve from "rollup-plugin-serve";
 import typescript from "rollup-plugin-typescript2";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import livereload from "rollup-plugin-livereload";
 import terser from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
+import copy from 'rollup-plugin-copy'
+
 
 const isProduction = process.env.NODE_ENV === "production";
 
 export default [
     {
-        input: "src/main.ts",
+        input: "src/devtools/main.ts",
         output: [
             {
-                file: "dist/bundle.js",
+                file: "build/devtools/devtools.js",
                 format: "esm",
             },
         ],
@@ -26,18 +26,74 @@ export default [
                 extensions: [".css"],
                 extract: true,
                 minimize: isProduction,
-                // modules: true,
             }),
-            // !isProduction &&
-            //   serve({
-            //     open: false,
-            //     verbose: true,
-            //     contentBase: ["dist", "public"],
-            //     host: "localhost",
-            //     port: 10001,
-            //   }),
-            // !isProduction && livereload(),
             isProduction && terser.terser(),
+            copy({
+                targets: [
+                  { src: 'src/devtools/devtools.html', dest: 'build/devtools' }
+                ]
+              })
         ],
     },
+
+    {
+        input: "src/popup/main.ts",
+        output: [
+            {
+                file: "build/popup/popup.js",
+                format: "esm",
+            },
+        ],
+        plugins: [
+            typescript(),
+            nodeResolve(),
+            postcss({
+                config: {
+                    path: "./postcss.config.js",
+                },
+                extensions: [".css"],
+                extract: true,
+                minimize: isProduction,
+            }),
+            isProduction && terser.terser(),
+            copy({
+                targets: [
+                  { src: 'src/popup/popup.html', dest: 'build/popup' }
+                ]
+              })
+        ],
+    },
+
+    {
+        input: "src/popup/main.ts",
+        output: [
+            {
+                file: "build/popup/devtools.js",
+                format: "esm",
+            },
+        ],
+        plugins: [
+            typescript(),
+            nodeResolve(),
+            postcss({
+                config: {
+                    path: "./postcss.config.js",
+                },
+                extensions: [".css"],
+                extract: true,
+                minimize: isProduction,
+            }),
+            isProduction && terser.terser(),
+            copy({
+                targets: [
+                  { src: 'src/background.html', dest: 'build' },
+                  { src: 'assets/images/**/*', dest: 'build/assets/images' },
+                  { src: 'manifest.json', dest: 'build' }
+                ]
+              })
+        ],
+    },
+
 ];
+
+//

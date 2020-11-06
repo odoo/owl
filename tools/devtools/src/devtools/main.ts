326c69d8e6f32066cef167100a4027d561d4fb3e
Simon Genin (ges) <ges@odoo.com>
2020-11-06 12:45:13 +0100

    wip

--- tools/devtools/src/devtools/main.ts
+++ tools/devtools/src/devtools/main.ts
@@ -4,8 +4,6 @@ import { utils } from "@odoo/owl";
 import "../main.css";
 
 chrome.devtools.panels.create("Owl", "MyPanelIcon.png", "devtools/devtools.html", function (panel) {
-    console.log("devtools.js is reading, owl starts...");
-
     (async () => {
         const app = new DevtoolsApp();
         await utils.whenReady();

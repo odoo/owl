efdad46afd48e0eda6c96c22682a2a4c8cd2d714
Simon Genin (ges) <ges@odoo.com>
2020-11-16 15:04:53 +0100

    wip

--- tools/devtools/src/devtools/components/devtools_app.ts
+++ tools/devtools/src/devtools/components/devtools_app.ts
@@ -1,11 +1,22 @@
 import { Component, tags } from "@odoo/owl";
+import Tab from './tab'
 
 const TEMPLATE = tags.xml/*xml*/ `
     <div t-name="DevtoolsApp">
-        Hello devtools app !
+
+        <nav class="flex py-1 border-b-2 border-gray-200">
+            
+            <Tab active="true" name="'Component Tree'" />
+            <Tab active="false" name="'Events'" />
+
+        </nav>
+
     </div>
 `;
 
 export class DevtoolsApp extends Component {
     static template = TEMPLATE;
+
+    static components = { Tab }
+
 }

e2aeed77be7e8f00708fe372ac995f4fb63740f8
Géry Debongnie <ged@odoo.com>
2021-07-14 16:23:57 +0200

    comment and remove useless code

--- src/core/scheduler.ts
+++ src/core/scheduler.ts
@@ -35,11 +35,11 @@ export class Scheduler {
    */
   flush() {
     this.tasks.forEach((fiber) => {
-      if (fiber.isCompleted) {
-        // leave the promise pending???
-        this.tasks.delete(fiber);
-        return;
-      }
+      // if (fiber.isCompleted) {
+      //   // leave the promise pending???
+      //   this.tasks.delete(fiber);
+      //   return;
+      // }
       if (fiber.counter === 0) {
         if (!fiber.error) {
           fiber.complete();
@@ -48,21 +48,6 @@ export class Scheduler {
         this.tasks.delete(fiber);
       }
     });
-
-    // let tasks = this.tasks.[...this.tasks];
-    // this.tasks = [];
-    // console.log(this.tasks)
-    // tasks = tasks.filter((fiber) => {
-    //   if (fiber.counter === 0) {
-    //     if (!fiber.error) {
-    //       fiber.complete();
-    //     }
-    //     fiber.resolve();
-    //     return false;
-    //   }
-    //   return true;
-    // });
-    // this.tasks = tasks.concat(this.tasks);
     if (this.tasks.size === 0) {
       this.stop();
     }

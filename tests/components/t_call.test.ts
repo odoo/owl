f5e241f5a0273d405ee0fb80013e88a3e7fbca76
Géry Debongnie <ged@odoo.com>
2021-07-17 11:03:17 +0200

    readd some tests

--- tests/components/t_call.test.ts
+++ tests/components/t_call.test.ts
@@ -1,4 +1,4 @@
-import { Component, mount, useState, xml } from "../../src/index";
+import { App, Component, mount, useState, xml } from "../../src/index";
 import { addTemplate, makeTestFixture, nextTick, snapshotEverything } from "../helpers";
 
 snapshotEverything();
@@ -27,5 +27,29 @@ describe("t-call", () => {
     app.current.template = "bar";
     await nextTick();
     expect(fixture.innerHTML).toBe("bar");
+  });
+
+  test("sub components in two t-calls", async () => {
+    class Child extends Component {
+      static template = xml`<span><t t-esc="props.val"/></span>`;
+    }
+
+    class Parent extends Component {
+      static template = xml`
+          <t t-if="state.val===1">
+            <t t-call="sub"/>
+          </t>
+          <div t-else=""><t t-call="sub"/></div>`;
+      static components = { Child };
+      state = useState({ val: 1 });
+    }
+    const app = new App(Parent);
+    app.addTemplate("sub", `<Child val="state.val"/>`);
+
+    const parent = await app.mount(fixture);
+    expect(fixture.innerHTML).toBe("<span>1</span>");
+    parent.state.val = 2;
+    await nextTick();
+    expect(fixture.innerHTML).toBe("<div><span>2</span></div>");
   });
 });

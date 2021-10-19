98bb2842d89cf811cf244798fcc325895a9b4318
Géry Debongnie <ged@odoo.com>
2021-10-19 16:45:04 +0200

    [FIX] run prettier on codebase

--- tests/qweb/qweb_memory.test.ts
+++ tests/qweb/qweb_memory.test.ts
@@ -5,10 +5,7 @@ describe("memory", () => {
   test("t-foreach does not leak stuff in global scope", () => {
     let qweb = new QWeb();
     const initialNumberOfGlobals = Object.keys(window).length;
-    qweb.addTemplate(
-      "test",
-      `<p><t t-foreach="[3, 2, 1]" t-as="item"><t t-esc="item"/></t></p>`
-    );
+    qweb.addTemplate("test", `<p><t t-foreach="[3, 2, 1]" t-as="item"><t t-esc="item"/></t></p>`);
     const result = renderToString(qweb, "test");
     const expected = `<p>321</p>`;
     expect(result).toBe(expected);

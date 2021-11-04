ae71db28e49853dba539d879046098c09a0a0f57
Lucas Perais (lpe) <lpe@odoo.com>
2021-11-04 13:46:49 +0100

    [IMP] qweb: compiler: support t-key on node and component without t-foreach

--- src/qweb/inline_expressions.ts
+++ src/qweb/inline_expressions.ts
@@ -25,10 +25,9 @@
 // Misc types, constants and helpers
 //------------------------------------------------------------------------------
 
-const RESERVED_WORDS =
-  "true,false,NaN,null,undefined,debugger,console,window,in,instanceof,new,function,return,this,eval,void,Math,RegExp,Array,Object,Date".split(
-    ","
-  );
+const RESERVED_WORDS = "true,false,NaN,null,undefined,debugger,console,window,in,instanceof,new,function,return,this,eval,void,Math,RegExp,Array,Object,Date".split(
+  ","
+);
 
 const WORD_REPLACEMENT: { [key: string]: string } = Object.assign(Object.create(null), {
   and: "&&",
--- tests/qweb/t_key.test.ts
+++ tests/qweb/t_key.test.ts
@@ -1,4 +1,5 @@
-import { renderToString, snapshotEverything } from "../helpers";
+import { renderToString, renderToBdom, snapshotEverything, makeTestFixture } from "../helpers";
+import { mount, patch } from "../../src/blockdom/index";
 
 snapshotEverything();
 
@@ -8,6 +9,28 @@ describe("t-key", () => {
     expect(renderToString(template, { beer: { id: 12, name: "Chimay Rouge" } })).toBe(
       "<div>Chimay Rouge</div>"
     );
+  });
+
+  test("can use t-key directive on a node as a function", () => {
+    const template = `<div t-key="getKey(beer)"><t t-esc="beer.name"/></div>`;
+    const getKey = (arg: any) => arg.id;
+    expect(renderToString(template, { getKey, beer: { id: 12, name: "Chimay Rouge" } })).toBe(
+      "<div>Chimay Rouge</div>"
+    );
+  });
+
+  test("can use t-key directive on a node 2", async () => {
+    const template = `<div t-key="beer.id"><t t-esc="beer.name"/></div>`;
+    const bd = renderToBdom(template, { beer: { id: 12, name: "Chimay Rouge" } });
+    const fixture = makeTestFixture();
+    await mount(bd, fixture);
+    const div = fixture.firstChild;
+
+    expect((div as HTMLElement).outerHTML).toBe("<div>Chimay Rouge</div>");
+    const bd2 = renderToBdom(template, { beer: { id: 13, name: "Chimay Rouge" } });
+    await patch(bd, bd2);
+    expect(div !== fixture.firstChild).toBeTruthy();
+    expect((div as HTMLElement).outerHTML).toBe("<div>Chimay Rouge</div>");
   });
 
   test("t-key directive in a list", () => {

030cc2f08adeefe3d046f551e795dbfe9755ddb3
Bruno Boi <boi@odoo.com>
2022-07-06 12:07:20 +0200

    wip [FIX] parser: account for CR/LF text nodes

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -258,16 +258,12 @@ function parseTNode(node: Element, ctx: ParsingContext): AST | null {
 // -----------------------------------------------------------------------------
 // Text and Comment Nodes
 // -----------------------------------------------------------------------------
-const lineBreakRE = /[\r\n]/;
-const whitespaceRE = /\s+/g;
+const whitespaceRE = /[^\S\r\n]+/g;
 
 function parseTextCommentNode(node: Node, ctx: ParsingContext): AST | null {
   if (node.nodeType === Node.TEXT_NODE) {
     let value = node.textContent || "";
     if (!ctx.inPreTag) {
-      if (lineBreakRE.test(value) && !value.trim()) {
-        return null;
-      }
       value = value.replace(whitespaceRE, " ");
     }
 

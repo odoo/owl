1c08db6bf22a472dc71f52e7501f094f13eac511
Julien <julien.carion25@gmail.com>
2023-01-11 11:47:42 +0100

    bigger icons in search bar, html selector and smoother select

--- tools/devtools/src/content.js
+++ tools/devtools/src/content.js
@@ -1,5 +1,13 @@
 window.addEventListener("message", function (event) {
-  if(event.data.type && (event.data.type === "Flush")){
-    chrome.runtime.sendMessage({ type: "Flush" })
+  if(event.data.type){
+    if(event.data.type === "owlDevtools__Flush"){
+      chrome.runtime.sendMessage({ type: "Flush" })
+    }
+    else if(event.data.type === "owlDevtools__SelectElement"){
+      chrome.runtime.sendMessage({ type: "SelectElement", path: event.data.path })
+    }
+    else if(event.data.type === "owlDevtools__StopSelector"){
+      chrome.runtime.sendMessage({ type: "StopSelector" })
+    }
   }
 }, false);

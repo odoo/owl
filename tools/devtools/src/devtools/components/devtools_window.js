974321136bc2a6bb6b6e0d4d2039ad3258404472
Julien <julien.carion25@gmail.com>
2023-02-02 10:01:07 +0100

    Navbar display, more events and go to component

--- tools/devtools/src/devtools/components/devtools_window.js
+++ tools/devtools/src/devtools/components/devtools_window.js
@@ -6,29 +6,50 @@ import { Events } from './events/events';
 export class DevtoolsWindow extends Component {
   setup(){
     this.state = useState({
-      page: "ComponentsTree"
+      page: "ComponentsTree",
+      selectedPath: [],
+      activeRecorder: false,
+      events: [],
+      owlStatus: true,
     })
+    onMounted(() => {
+      // Connect to the port to communicate to the background script
+      chrome.runtime.onConnect.addListener((port) => {
+        port.onMessage.addListener((msg) => {
+          // When message of type Event is received, add the received event to the list
+          if (msg.type === "Event"){
+            if(this.state.activeRecorder)
+              this.state.events = [...this.state.events, msg.data];
+          }
+        });
+      });
+    });
+  }
+
+  updateOwlStatus(status){
+    this.state.owlStatus = status;
+  }
+
+  toggleRecorder(){
+    this.state.activeRecorder = !this.state.activeRecorder;
+  }
+
+  clearEvents(){
+    this.state.events = [];
   }
   
   // Remove the highlight on the DOM element correponding to the component
   removeHighlight(ev){
-    const script = "__OWL__DEVTOOLS_GLOBAL_HOOK__.removeHighlights()"
+    const script = "__OWL__DEVTOOLS_GLOBAL_HOOK__.removeHighlights()";
     chrome.devtools.inspectedWindow.eval(script);
   }
 
   switchTab(componentName) {
-      this.state.page = componentName
+    this.state.page = componentName;
   };
 
-  get selectPage() { 
-    switch (this.state.page) {
-      case 'Events':
-        return Events;
-      case 'ComponentsTree':
-        return ComponentsTree;
-      default: 
-        return ComponentsTree;
-    }
+  selectComponent(path){
+    this.state.selectedPath = path;
   }
   
   static props = [];
--- tools/devtools/src/devtools/components/events/events.js
+++ tools/devtools/src/devtools/components/events/events.js
@@ -1,33 +1,19 @@
-const { Component, useState, onMounted } = owl
+const { Component, onWillDestroy, onMounted } = owl
+import { Event } from "./event";
 
 export class Events extends Component { 
   setup() {
-    this.state = useState({
-      activeRecorder: false,
-      events: []
-    })
-
-    onMounted(async () => {
-      // Connect to the port to communicate to the background script
-      chrome.runtime.onConnect.addListener((port) => {
-        port.onMessage.addListener((msg) => {
-          // When message of type Event is received, add the received event to the list
-          if (msg.type === "Event"){
-            if(this.state.activeRecorder)
-              this.state.events = [...this.state.events, msg.data];
-          }
-        });
-      });
-    });
   }
 
   toggleRecording(ev){
-    this.state.activeRecorder = !this.state.activeRecorder;
+    this.props.toggleRecorder();
   }
 
   clearConsole(ev){
-    this.state.events = [];
+    this.props.clearEvents();
   }
 
   static template = "devtools.Events";
+
+  static components = { Event };
 }

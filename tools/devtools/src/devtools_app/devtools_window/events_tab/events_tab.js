4b52c60bcb5091120c259542880b5b2a0c553a28
Julien <julien.carion25@gmail.com>
2023-02-09 11:27:29 +0100

    temp serachbar

--- tools/devtools/src/devtools_app/devtools_window/events_tab/events_tab.js
+++ tools/devtools/src/devtools_app/devtools_window/events_tab/events_tab.js
@@ -2,11 +2,12 @@ const { Component, onWillDestroy, onMounted } = owl;
 import { evalInWindow } from "../../../utils";
 import { useStore } from "../../store/store";
 import { Event } from "./event/event";
+import { EventSearchBar } from "./event_search_bar/event_search_bar";
 
 export class EventsTab extends Component {
   static template = "devtools.EventsTab";
 
-  static components = { Event };
+  static components = { Event, EventSearchBar };
 
   setup() {
     this.store = useStore();

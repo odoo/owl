f108419a32a3e510c107c2e7d571c020137113a4
Julien <julien.carion25@gmail.com>
2023-02-15 13:44:04 +0100

    Trace origin of non root events

--- tools/devtools/src/devtools_app/devtools_window/events_tab/event/event.js
+++ tools/devtools/src/devtools_app/devtools_window/events_tab/event/event.js
@@ -1,3 +1,4 @@
+import { evalInWindow } from "../../../../utils";
 import { useStore } from "../../../store/store";
 
 const { Component, useState, onMounted } = owl;
@@ -14,7 +15,38 @@ export class Event extends Component {
     this.store.switchTab("ComponentsTab");
   }
 
-  addInstanceToBlacklist() {}
+  goToOriginComponent() {
+    this.store.selectComponent(this.props.origin.path);
+    this.store.switchTab("ComponentsTab");
+  }
 
-  addComponentToBlacklist() {}
+  toggleDisplay() {
+    this.store.events[this.props.id].toggled = !this.store.events[this.props.id].toggled;
+  }
+
+  hoverComponent(ev) {
+    evalInWindow("highlightComponent", [JSON.stringify(this.props.path)], this.store.activeFrame);
+  }
+
+  get minimizedKey() {
+    const split = this.props.key.split("__");
+    let key;
+    if (split.length > 2) {
+      key = this.props.key.substring(4 + split[1].length, this.props.key.length);
+    } else {
+      key = "";
+    }
+    return key;
+  }
+
+  get originMinimizedKey() {
+    const split = this.props.origin.key.split("__");
+    let key;
+    if (split.length > 2) {
+      key = this.props.origin.key.substring(4 + split[1].length, this.props.origin.key.length);
+    } else {
+      key = "";
+    }
+    return key;
+  }
 }

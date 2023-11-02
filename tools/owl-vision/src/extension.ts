34489a249489abb25d23cdf6142d040cb6e32389
Bastien Fafchamps (bafa) <bafa@odoo.com>
2023-11-02 13:33:20 +0100

    [IMP] owl-vision: syntax scripts, single quotes attributes and slot props highlight and switch below command
    This commit adds the following:
    
    - Syntax builder scripts to make syntaxes easier to read and edit
    - Syntax highlight in single quote attributes
    - Syntax highlight for slot props
    - Basic syntax highlight for xpaths
    - Added `Switch Below` command
    
    This commit fixes the following:
    
    - Using `Switch Besides` or `Switch Below` does not open a new panel if one was already open
    - Fixed missing space in component's snippet indentation

--- tools/owl-vision/src/extension.ts
+++ tools/owl-vision/src/extension.ts
@@ -1,12 +1,14 @@
 import * as vscode from 'vscode';
 import { Search } from './search';
 import { ComponentDefinitionProvider } from './definiton_providers';
+import { OpenDirection } from './utils';
 
 export async function activate(context: vscode.ExtensionContext) {
     const search = new Search();
 
-    context.subscriptions.push(vscode.commands.registerCommand('owl-vision.switch', () => search.switchCommand()));
-    context.subscriptions.push(vscode.commands.registerCommand('owl-vision.switch-besides', () => search.switchCommand(true)));
+    context.subscriptions.push(vscode.commands.registerCommand('owl-vision.switch', () => search.switch()));
+    context.subscriptions.push(vscode.commands.registerCommand('owl-vision.switch-besides', () => search.switch(OpenDirection.Besides)));
+    context.subscriptions.push(vscode.commands.registerCommand('owl-vision.switch-below', () => search.switch(OpenDirection.Below)));
     context.subscriptions.push(vscode.commands.registerCommand('owl-vision.find-component', () => search.findComponentCommand()));
     context.subscriptions.push(vscode.commands.registerCommand('owl-vision.find-template', () => search.findTemplateCommand()));
 

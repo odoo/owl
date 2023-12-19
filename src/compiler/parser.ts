5ef405293a0f1cb04b9354a0b882457eb5daf390
Samuel Degueldre <sad@odoo.com>
2023-12-19 08:17:27 +0100

    [REF] parser, template_set: factor out parseXML function
    For some reason the code of parseXML was duplicated, despite being
    exactly the same except for some whitespace. Move it out into a common
    utils file.
    
    closes #1569

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -1,4 +1,5 @@
 import { OwlError } from "../common/owl_error";
+import { parseXML } from "../common/utils";
 
 // -----------------------------------------------------------------------------
 // AST Type definition
@@ -971,41 +972,4 @@ function normalizeTEscTOut(el: Element) {
 function normalizeXML(el: Element) {
   normalizeTIf(el);
   normalizeTEscTOut(el);
-}
-
-/**
- * Parses an XML string into an XML document, throwing errors on parser errors
- * instead of returning an XML document containing the parseerror.
- *
- * @param xml the string to parse
- * @returns an XML document corresponding to the content of the string
- */
-function parseXML(xml: string): XMLDocument {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(xml, "text/xml");
-  if (doc.getElementsByTagName("parsererror").length) {
-    let msg = "Invalid XML in template.";
-    const parsererrorText = doc.getElementsByTagName("parsererror")[0].textContent;
-    if (parsererrorText) {
-      msg += "\nThe parser has produced the following error message:\n" + parsererrorText;
-      const re = /\d+/g;
-      const firstMatch = re.exec(parsererrorText);
-      if (firstMatch) {
-        const lineNumber = Number(firstMatch[0]);
-        const line = xml.split("\n")[lineNumber - 1];
-        const secondMatch = re.exec(parsererrorText);
-        if (line && secondMatch) {
-          const columnIndex = Number(secondMatch[0]) - 1;
-          if (line[columnIndex]) {
-            msg +=
-              `\nThe error might be located at xml line ${lineNumber} column ${columnIndex}\n` +
-              `${line}\n${"-".repeat(columnIndex - 1)}^`;
-          }
-        }
-      }
-    }
-    throw new OwlError(msg);
-  }
-
-  return doc;
 }

0e67c593fa0f79bd97304edcda28296cbd7ed32a
Géry Debongnie <ged@odoo.com>
2025-12-04 13:26:40 +0100

    [REF] reactivity: split code into multiple files

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -39,8 +39,11 @@ export { Component } from "./component";
 export type { ComponentConstructor } from "./component";
 export { useComponent } from "./component_node";
 export { status } from "./status";
-export { proxy, markRaw, toRaw } from "./reactivity/reactivity";
-export { effect, withoutReactivity, derived, signal } from "./reactivity/signals";
+export { proxy, markRaw, toRaw } from "./reactivity/proxy";
+export { withoutReactivity } from "./reactivity/computations";
+export { signal } from "./reactivity/signal";
+export { derived } from "./reactivity/derived";
+export { effect } from "./reactivity/effect";
 export { useEffect, useEnv, useListener, useRef, useChildSubEnv, useSubEnv } from "./hooks";
 export { batched, EventBus, htmlEscape, whenReady, markup } from "./utils";
 export {
---
+++ src/runtime/reactivity/signal.ts
@@ -0,0 +1,47 @@
+import { onReadAtom, onWriteAtom, Atom, Opts } from "./computations";
+
+type SignalFunction<T> = () => T;
+
+export interface Signal<T> extends SignalFunction<T> {
+  /**
+   * Update the value of the signal with a new value. If the new value is different
+   * from the previous values, all computations that depends on this signal will
+   * be invalidated, and effects will rerun.
+   */
+  set(value: T): void;
+  /**
+   * Call the updater function (if given) to update the signal value.
+   * If the updater value is not given, then all computations that depends on
+   * this signal will be invalidated and effects will rerun.
+   */
+  update(updater?: (value: T) => T): void;
+}
+
+export function signal<T>(value: T, opts?: Opts): Signal<T> {
+  const atom: Atom<T> = {
+    value,
+    observers: new Set(),
+    name: opts?.name,
+  };
+  const read = () => {
+    onReadAtom(atom);
+    return atom.value;
+  };
+  const write = (newValue: T | ((prevValue: T) => T)) => {
+    if (typeof newValue === "function") {
+      newValue = (newValue as (prevValue: T) => T)(atom.value);
+    }
+    if (Object.is(atom.value, newValue)) return;
+    atom.value = newValue;
+    onWriteAtom(atom);
+  };
+  read.set = write;
+  read.update = (updater?: (value: T) => T) => {
+    if (updater) {
+      write(updater(atom.value));
+    } else {
+      onWriteAtom(atom);
+    }
+  };
+  return read;
+}

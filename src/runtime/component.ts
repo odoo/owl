df82e05ca5d3d7339f774e89a655307fd89e8871
Michaël Mattiello <mcm@odoo.com>
2025-12-09 11:51:35 +0100

    [IMP] hooks: remove env related hooks

--- src/runtime/component.ts
+++ src/runtime/component.ts
@@ -4,29 +4,23 @@ import type { ComponentNode } from "./component_node";
 //  Component Class
 // -----------------------------------------------------------------------------
 
-export type Props = { [key: string]: any };
-
 interface StaticComponentProperties {
   template: string;
   defaultProps?: any;
   components?: { [componentName: string]: ComponentConstructor };
 }
 
-export interface ComponentConstructor<P extends Props = any, E = any>
-  extends StaticComponentProperties {
-  new (env: E, node: ComponentNode): Component<P, E>;
+export interface ComponentConstructor extends StaticComponentProperties {
+  new (node: ComponentNode): Component;
 }
 
-export class Component<Props = any, Env = any> {
+export class Component {
   static template: string = "";
   static defaultProps?: any;
 
-  __props: Props = {} as any; // TODO: remove. it's just to keep types for now
-  env: Env;
   __owl__: ComponentNode;
 
-  constructor(env: Env, node: ComponentNode) {
-    this.env = env;
+  constructor(node: ComponentNode) {
     this.__owl__ = node;
   }
 

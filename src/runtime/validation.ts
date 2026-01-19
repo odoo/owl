a4a31394a8a6c9b500ef8e0621df98cf043d3bb3
Michaël Mattiello <mcm@odoo.com>
2026-01-19 13:09:20 +0100

    implement validation

--- src/runtime/validation.ts
+++ src/runtime/validation.ts
@@ -1,9 +1,28 @@
 import { OwlError } from "../common/owl_error";
 
+export interface ValidationIssue {
+  message: string;
+  path?: PropertyKey[];
+  received?: any;
+  [K: string]: any;
+}
+
+export interface ValidationContext {
+  addIssue(issue: ValidationIssue): void;
+  isValid: boolean;
+  issueDepth: number;
+  mergeIssues(issues: ValidationIssue[]): void;
+  path: PropertyKey[];
+  validate(type: any): void;
+  value: any;
+  withIssues(issues: ValidationIssue[]): ValidationContext;
+  withKey(key: PropertyKey): ValidationContext;
+}
+
 export function assertType(
   value: any,
   validation: any,
-  errorMessage = "Value does not match the type"
+  errorMessage = "Value does not match the type",
 ): void {
   const issues = validateType(value, validation);
   if (issues.length) {
@@ -15,12 +34,52 @@ export function assertType(
         }
         return value;
       },
-      2
+      2,
     );
     throw new OwlError(`${errorMessage}\n${issueStrings}`);
   }
 }
 
-export function validateType(value: any, validation: any): any[] {
-  return (validation as any)(value);
+function createContext(
+  issues: ValidationIssue[],
+  value: any,
+  path: PropertyKey[],
+  parent?: ValidationContext,
+): ValidationContext {
+  return {
+    issueDepth: 0,
+    path,
+    value,
+    get isValid() {
+      return !issues.length;
+    },
+    addIssue(issue) {
+      issues.push({
+        received: this.value,
+        path: this.path,
+        ...issue,
+      });
+    },
+    mergeIssues(newIssues) {
+      issues.push(...newIssues);
+    },
+    validate(type: any) {
+      type(this);
+      if (!this.isValid && parent) {
+        parent.issueDepth = this.issueDepth + 1;
+      }
+    },
+    withIssues(issues) {
+      return createContext(issues, this.value, this.path, this);
+    },
+    withKey(key) {
+      return createContext(issues, this.value[key], this.path.concat(key), this);
+    },
+  };
+}
+
+export function validateType(value: any, validation: any): ValidationIssue[] {
+  const issues: ValidationIssue[] = [];
+  validation(createContext(issues, value, []));
+  return issues;
 }

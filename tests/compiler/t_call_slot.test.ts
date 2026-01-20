505a62d5f050dab46e6cf34896b107cf4d1f56ed
Michaël Mattiello <mcm@odoo.com>
2026-01-20 09:35:17 +0100

    fix t-slot + add test

--- tests/compiler/t_call_slot.test.ts
+++ tests/compiler/t_call_slot.test.ts
@@ -12,4 +12,15 @@ describe("t-call-slot", () => {
     const fn2 = compile(parsedTemplate);
     expect(fn2.toString()).toBe(fn1.toString());
   });
+
+  test("warn on t-slot", () => {
+    const template = `<t t-slot="default"/>`;
+    const parsedTemplate = parseXML(template).firstChild as Element;
+    const originalConsoleWarn = console.warn;
+    const mockConsoleWarn = jest.fn(() => {});
+    console.warn = mockConsoleWarn;
+    compile(parsedTemplate);
+    console.warn = originalConsoleWarn;
+    expect(mockConsoleWarn).toHaveBeenCalledWith("t-slot has been renamed t-call-slot.");
+  });
 });

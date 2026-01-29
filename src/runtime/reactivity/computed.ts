a8f70c527c2ef57c0740a79ab3fccf96f0e80ccb
Michaël Mattiello <mcm@odoo.com>
2026-01-29 11:44:03 +0100

    writable computed

--- src/runtime/reactivity/computed.ts
+++ src/runtime/reactivity/computed.ts
@@ -4,14 +4,26 @@ import {
   onWriteAtom,
   ReactiveValue,
   updateComputation,
+  WritableReactiveValue,
   createComputation,
 } from "./computations";
 
-export function computed<T>(fn: () => T): ReactiveValue<T> {
+type ReadonlyComputedTransform<T> = () => T;
+interface WritableComputedTransform<T> {
+  get(): T;
+  set(nextValue: T): void;
+}
+
+export function computed<T>(transform: WritableComputedTransform<T>): WritableReactiveValue<T>;
+export function computed<T>(transform: ReadonlyComputedTransform<T>): ReactiveValue<T>;
+export function computed(transform: any): any {
+  const get = typeof transform === "function" ? transform : transform.get;
+  const set = typeof transform === "function" ? null : transform.set;
+
   const computation = createComputation({
     compute: () => {
       onWriteAtom(computation);
-      return fn();
+      return get();
     },
     isDerived: true,
   });
@@ -22,6 +34,17 @@ export function computed<T>(fn: () => T): ReactiveValue<T> {
     return computation.value;
   }
   readComputed[atomSymbol] = computation;
+
+  if (set) {
+    readComputed.set = function writeComputed(nextValue: any) {
+      if (Object.is(nextValue, computation.value)) {
+        return;
+      }
+      computation.value = nextValue;
+      set(nextValue);
+      onWriteAtom(computation);
+    };
+  }
 
   return readComputed;
 }

622c99b4ea838d3c6b86bafe6f35f7e0efd6e74f
Michaël Mattiello <mcm@odoo.com>
2026-02-02 09:40:04 +0100

    rename input to config

--- src/runtime/plugin_hooks.ts
+++ src/runtime/plugin_hooks.ts
@@ -22,26 +22,26 @@ export function plugin<T extends PluginConstructor>(pluginType: T): PluginInstan
   return plugin;
 }
 
-declare const inputSymbol: unique symbol;
-export type PluginInput<K extends string, T> = T & { [inputSymbol]: true };
+declare const configSymbol: unique symbol;
+export type PluginConfig<K extends string, T> = T & { [configSymbol]: true };
 
-export type GetPluginInputs<T> = {
-  [P in keyof T as T[P] extends PluginInput<infer K, infer I /* magic! */>
+export type GetPluginConfig<T> = {
+  [P in keyof T as T[P] extends PluginConfig<infer K, infer I /* magic! */>
     ? K
-    : never]: T[P] extends PluginInput<string, infer I> ? I : never;
+    : never]: T[P] extends PluginConfig<string, infer I> ? I : never;
 };
 
-export function input<const K extends string, T>(name: K, type?: T): PluginInput<K, T> {
+export function config<const K extends string, T>(name: K, type?: T): PluginConfig<K, T> {
   const { app, manager } = getContext("plugin");
-  const value = manager.inputs[name];
+  const value = manager.config[name];
   if (app.dev) {
     assertType(value, type, "Plugin input value does not match the type");
   }
   return value;
 }
 
-type GetPluginsInputs<T extends PluginConstructor[]> = {
-  [I in keyof T]: (x: GetPluginInputs<InstanceType<T[I]>>) => void;
+type GetPluginsConfig<T extends PluginConstructor[]> = {
+  [I in keyof T]: (x: GetPluginConfig<InstanceType<T[I]>>) => void;
 } extends {
   [K: number]: (x: infer I) => void;
 }
@@ -51,11 +51,11 @@ type PrettifyShape<T> = T extends Function ? T : { [K in keyof T]: T[K] };
 
 export function providePlugins<const P extends PluginConstructor[]>(
   pluginConstructors: P,
-  inputs?: PrettifyShape<GetPluginsInputs<P>>
+  config?: PrettifyShape<GetPluginsConfig<P>>
 ) {
   const { node } = getContext("component");
 
-  const manager = new PluginManager(node.app, { parent: node.pluginManager, inputs });
+  const manager = new PluginManager(node.app, { parent: node.pluginManager, config });
   node.pluginManager = manager;
   onWillDestroy(() => manager.destroy());
 

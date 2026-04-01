62ad60c22622421d98d9df5e4b6e78fbb31e1af5
Géry Debongnie <ged@odoo.com>
2026-04-01 16:40:52 +0200

    [DOC] playground: add a final step to the hibou os tutorial
    We add here a workspace switcher step, because it makes the project more
    interesting.

---
+++ docs/playground/samples/tutorials/hibou_os/15/workspace_switcher.js
@@ -0,0 +1,8 @@
+import { Component, plugin } from "@odoo/owl";
+import { WindowManagerPlugin } from "../window/window_manager_plugin";
+
+export class WorkspaceSwitcher extends Component {
+    static template = "hibou.WorkspaceSwitcher";
+
+    wm = plugin(WindowManagerPlugin);
+}
